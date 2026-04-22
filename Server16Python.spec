e3acf2cfd84046da7a8c42e3192e77fc5994cb19
igor vinicius <igorviniciusfreitasouza@gmail.com>
2026-04-22 07:34:42 -0300

    Use root icon file in Windows build

--- Server16Python.spec
+++ Server16Python.spec
@@ -35,5 +35,5 @@ exe = EXE(
     target_arch=None,
     codesign_identity=None,
     entitlements_file=None,
-    icon=['assets\\server16.ico'],
+    icon=['server16.ico'],
 )
--- build_exe.bat
+++ build_exe.bat
@@ -6,19 +6,18 @@ set "APP_NAME=Server16Python"
 set "WORKPATH=build\pyinstaller"
 set "DISTPATH=dist"
 
+if not exist "server16.ico" (
+  echo.
+  echo Arquivo de icone nao encontrado: %~dp0server16.ico
+  exit /b 1
+)
+
 pyinstaller ^
   --noconfirm ^
   --clean ^
-  --windowed ^
-  --onefile ^
-  --icon "assets\server16.ico" ^
-  --name "%APP_NAME%" ^
   --distpath "%DISTPATH%" ^
   --workpath "%WORKPATH%" ^
-  --hidden-import "clr" ^
-  --add-data "server16_py\offsets.json;server16_py" ^
-  --add-data "bin\FifaLibrary14.dll;bin" ^
-  main.py
+  "Server16Python.spec"
 
 echo.
 echo Build finalizado.
